97e5edae81a71a64a2e888b31a41df5939291f2c
Kathrin Geilmann <geilmann@cs.uni-kl.de>
2012-01-16 13:07:28 +0100

    Removed patch for universal compilation.
    As far as I can see, the patch is included in the 
    current version. UNTESTED

--- Library/Formula/cairomm.rb
+++ Library/Formula/cairomm.rb
@@ -17,13 +17,6 @@ class Cairomm < Formula
   homepage 'http://cairographics.org/cairomm/'
   md5 '9c63fb1c04c8ecd3c5e6473075b8c39f'
 
-  # patch for universal compilation from:
-  # http://trac.macports.org/browser/trunk/dports/graphics/cairomm/files/patch-quartz-lp64.diff
-  # was needed for cairomm 1.8.4, unknown wether it is still needed
-  #def patches
-  #  { :p0 => DATA }
-  #end
-
   depends_on 'pkg-config' => :build
   depends_on 'libsigc++'
 
@@ -37,51 +30,3 @@ class Cairomm < Formula
     system "make install"
   end
 end
-
-
-__END__
-diff -urN cairomm/quartz_font.cc cairomm-1.8.2/cairomm/quartz_font.cc
---- cairomm/quartz_font.cc	2008-12-20 18:37:46.000000000 +0100
-+++ cairomm/quartz_font.cc	2009-09-20 17:45:13.000000000 +0200
-@@ -30,21 +30,23 @@
-   check_object_status_and_throw_exception(*this);
- }
- 
--QuartzFontFace::QuartzFontFace(ATSUFontID font_id) :
--  FontFace(cairo_quartz_font_face_create_for_atsu_font_id(font_id), true)
-+RefPtr<QuartzFontFace> QuartzFontFace::create(CGFontRef font)
- {
--  check_object_status_and_throw_exception(*this);
-+  return RefPtr<QuartzFontFace>(new QuartzFontFace(font));
- }
- 
--RefPtr<QuartzFontFace> QuartzFontFace::create(CGFontRef font)
-+#if !__LP64__
-+QuartzFontFace::QuartzFontFace(ATSUFontID font_id) :
-+  FontFace(cairo_quartz_font_face_create_for_atsu_font_id(font_id), true)
- {
--  return RefPtr<QuartzFontFace>(new QuartzFontFace(font));
-+  check_object_status_and_throw_exception(*this);
- }
- 
- RefPtr<QuartzFontFace> QuartzFontFace::create(ATSUFontID font_id)
- {
-   return RefPtr<QuartzFontFace>(new QuartzFontFace(font_id));
- }
-+#endif
- 
- }
- 
-diff cairomm/quartz_font.h cairomm-1.8.2/cairomm/quartz_font.h
---- cairomm/quartz_font.h	2008-12-20 18:37:46.000000000 +0100
-+++ cairomm/quartz_font.h	2009-09-20 17:46:25.000000000 +0200
-@@ -54,7 +54,9 @@
-    *
-    * @since 1.8
-    */
-+# if !__LP64__
-   static RefPtr<QuartzFontFace> create(ATSUFontID font_id);
-+# endif
- 
- 
- protected:

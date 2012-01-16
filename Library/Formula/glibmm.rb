ed708046c1f18a0dec3a37a8c834e2d0003f2a5c
Kathrin Geilmann <geilmann@cs.uni-kl.de>
2012-01-16 13:21:58 +0100

    Updated to 2.30.1

--- Library/Formula/glibmm.rb
+++ Library/Formula/glibmm.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Glibmm < Formula
-  url 'http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.22/glibmm-2.22.2.tar.gz'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.30/glibmm-2.30.1.tar.bz2'
   homepage 'http://www.gtkmm.org/'
-  md5 '90ff0a5bd5987431cf36f0aa0160a753'
+  md5 '9b333de989287c563334faa88a11fc21'
 
   depends_on 'pkg-config' => :build
   depends_on 'libsigc++'
